8ba8079398a748f172eb501e2c79cdbb49cd45f2
Nick <mnickolas.1@gmail.com>
2023-04-03 08:44:39 +0300

    Add files via upload

---
+++ blackholes/bh_feedback.c
@@ -0,0 +1,247 @@
+/*!
+ * \copyright   This file is part of the public version of the AREPO code.
+ * \copyright   Copyright (C) 2009-2019, Max-Planck Institute for Astrophysics
+ * \copyright   Developed by Volker Springel (vspringel@MPA-Garching.MPG.DE) and
+ *              contributing authors.
+ * \copyright   Arepo is free software: you can redistribute it and/or modify
+ *              it under the terms of the GNU General Public License as published by
+ *              the Free Software Foundation, either version 3 of the License, or
+ *              (at your option) any later version.
+ *
+ *              Arepo is distributed in the hope that it will be useful,
+ *              but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *              MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *              GNU General Public License for more details.
+ *
+ *              A copy of the GNU General Public License is available under
+ *              LICENSE as part of this program.  See also
+ *              <https://www.gnu.org/licenses/>.
+ *
+ * \file        src/init/density.c
+ * \date        05/2018
+ * \brief       SPH density computation and smoothing length determination.
+ * \details     This file contains the "first SPH loop", where the SPH
+ *              densities and smoothing lengths are calculated.
+ *              In Arepo, this is used in setup_smoothinglengths() (init.c) to
+ *              get an initial guess for MaxDelaunayRadius.
+ *              Note that the SPH density is NOT used in the subsequent
+ *              hydrodynamics calculation, but the density is either set by the
+ *              initial conditions explicitly (DENSITY_AS_MASS_IN_INPUT) or
+ *              calculated by the mass given in the initial conditions divided
+ *              by the volume of the cell calculated by the Voronoi
+ *              tessellation algorithm.
+ *              contains functions:
+ *                static void particle2in(data_in * in, int i, int firstnode)
+ *                static void out2particle(data_out * out, int i, int mode)
+ *                static void kernel_local(void)
+ *                static void kernel_imported(void)
+ *                void density(void)
+ *                static int density_evaluate(int target, int mode, int
+ *                  threadid)
+ *                int density_isactive(int n)
+ *
+ * \par Major modifications and contributions:
+ *
+ * - DD.MM.YYYY Description
+ * - 04.05.2018 Prepared file for public release -- Rainer Weinberger
+ */
+
+#include <gsl/gsl_math.h>
+#include <math.h>
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../main/allvars.h"
+#include "../main/proto.h"
+
+#include "../domain/domain.h"
+
+
+static int bh_feedback_evaluate(int target, int mode, int threadid);
+
+/*! \brief Local data structure for collecting particle/cell data that is sent
+ *         to other processors if needed. Type called data_in and static
+ *         pointers DataIn and DataGet needed by generic_comm_helpers2.
+ */
+typedef struct
+{
+  MyDouble Pos[3];
+  MyFloat Hsml;
+  int Firstnode;
+} data_in;
+
+static data_in *DataIn, *DataGet;
+
+/*! \brief Routine that fills the relevant particle/cell data into the input
+ *         structure defined above. Needed by generic_comm_helpers2.
+ *
+ *  \param[out] in Data structure to fill.
+ *  \param[in] i Index of particle in P and SphP arrays.
+ *  \param[in] firstnode First note of communication.
+ *
+ *  \return void
+ */
+static void particle2in(data_in *in, int i, int firstnode)
+{
+  in->Pos[0] = P[BhP[i].PID].Pos[0];
+  in->Pos[1] = P[BhP[i].PID].Pos[1];
+  in->Pos[2] = P[BhP[i].PID].Pos[2];
+  in->Hsml   = BhP[i].Hsml;
+
+  in->Firstnode = firstnode;
+}
+
+/*! \brief Local data structure that holds results acquired on remote
+ *         processors. Type called data_out and static pointers DataResult and
+ *         DataOut needed by generic_comm_helpers2.
+ */
+typedef struct
+{
+  MyDouble ngbmass;
+} data_out;
+
+static data_out *DataResult, *DataOut;
+
+/*! \brief Routine to store or combine result data. Needed by
+ *         generic_comm_helpers2.
+ *
+ *  \param[in] out Data to be moved to appropriate variables in global
+ *  particle and cell data arrays (P, SphP,...)
+ *  \param[in] i Index of particle in P and SphP arrays
+ *  \param[in] mode Mode of function: local particles or information that was
+ *  communicated from other tasks and has to be added locally?
+ *
+ *  \return void
+ */
+static void out2particle(data_out *out, int i, int mode)
+{
+  if(mode == MODE_LOCAL_PARTICLES) /* initial store */
+    {
+      BhP[i].NgbMass = out->ngbmass;
+    }
+  else /* combine */
+    {
+      BhP[i].NgbMass += out->ngbmass;
+    }
+}
+
+#include "../utils/generic_comm_helpers2.h"
+
+/*! \brief Routine that defines what to do with local particles.
+ *
+ *  Calls the *_evaluate function in MODE_LOCAL_PARTICLES.
+ *
+ *  \return void
+ */
+static void kernel_local(void)
+{
+  int idx;
+
+  {
+    int j, threadid = get_thread_num();
+
+    for(j = 0; j < NTask; j++)
+      Thread[threadid].Exportflag[j] = -1;
+
+    while(1)
+      {
+        if(Thread[threadid].ExportSpace < MinSpace)
+          break;
+
+        idx = NextParticle++;
+        
+        if(idx >= NumBh)
+          break;
+        
+        bh_feedback_evaluate(idx, MODE_LOCAL_PARTICLES, threadid);
+      }
+  }
+}
+
+/*! \brief Routine that defines what to do with imported particles.
+ *
+ *  Calls the *_evaluate function in MODE_IMPORTED_PARTICLES.
+ *
+ *  \return void
+ */
+static void kernel_imported(void)
+{
+  /* now do the particles that were sent to us */
+  int i, cnt = 0;
+  {
+    int threadid = get_thread_num();
+
+    while(1)
+      {
+        i = cnt++;
+
+        if(i >= Nimport)
+          break;
+
+        bh_feedback_evaluate(i, MODE_IMPORTED_PARTICLES, threadid);
+      }
+  }
+}
+
+
+void bh_feedback(void)
+{
+  generic_set_MaxNexport();
+
+  generic_comm_pattern(NumBh, kernel_local, kernel_imported);
+}
+
+
+
+static int bh_feedback_evaluate(int target, int mode, int threadid)
+{
+  int j, n;
+  int numnodes, *firstnode;
+  double h;
+  MyDouble mass;
+  MyDouble *pos;
+
+  mass=0;
+
+  data_in local, *target_data;
+  data_out out;
+
+  if(mode == MODE_LOCAL_PARTICLES)
+    {
+      particle2in(&local, target, 0);
+      target_data = &local;
+
+      numnodes  = 1;
+      firstnode = NULL;
+    }
+  else
+    {
+      target_data = &DataGet[target];
+
+      generic_get_numnodes(target, &numnodes, &firstnode);
+    }
+
+  pos = target_data->Pos;
+  h   = target_data->Hsml;
+
+  int nfound = ngb_treefind_variable_threads(pos, h, target, mode, threadid, numnodes, firstnode);
+
+  for(n = 0; n < nfound; n++)
+    {
+      j = Thread[threadid].Ngblist[n];
+      mass += P[j].Mass;
+    }
+    
+  out.ngbmass = mass;
+   
+  /* Now collect the result at the right place */
+  if(mode == MODE_LOCAL_PARTICLES)
+    out2particle(&out, target, MODE_LOCAL_PARTICLES);
+  else
+    DataResult[target] = out;
+
+  return 0;
+}
+
