c124f903b8a83d39e72195769f0b6d1312337c82
Nick <mnickolas.1@gmail.com>
2023-04-21 20:40:51 +0800

    call_update_accretion_rate in run

--- main/run.c
+++ main/run.c
@@ -384,6 +384,7 @@ void calculate_non_standard_physics_prior_mesh_construction(void)
   bh_density();
   update_bh_timesteps();
   update_list_of_active_bh_particles();
+  update_bh_accretion_rate();
    if(All.Time >= All.FeedbackTime)
     {   
       if(All.FeedbackFlag > 0)
