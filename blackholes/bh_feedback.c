e69544825ed68408e1e8ac12a8a28d6131e6b50c
Nick <mnickolas.1@gmail.com>
2023-06-08 10:02:11 +0800

    typo

--- blackholes/bh_feedback.c
+++ blackholes/bh_feedback.c
@@ -364,7 +364,7 @@ static int bh_ngb_feedback_evaluate(int target, int mode, int threadid)
               SphP[j].MomentumFeed  += All.Lambda * energyfeed / (CLIGHT / All.UnitVelocity_in_cm_per_s) * P[j].Mass / bh_rho * wk;
 /*Epsilon_r terms cancel out in the eddington rate formula*/
 
-              All.EnergyExchange[2] += energyfeed / (CLIGHT / All.UnitVelocity_in_cm_per_s) * P[j].Mass / bh_rho * wk;
+              All.EnergyExchange[2] += All.Lambda * energyfeed / (CLIGHT / All.UnitVelocity_in_cm_per_s) * P[j].Mass / bh_rho * wk;
 
               SphP[j].MomentumKickVector[0] = -dx;
               SphP[j].MomentumKickVector[1] = -dy;
