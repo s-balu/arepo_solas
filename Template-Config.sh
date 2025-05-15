2a6ef35f45bb5d5f84ab592b49748b04e8cf6449
Balu Sreedhar <balu.srdhr@gmail.com>
2025-05-15 18:43:45 +1000

    update config file

--- Template-Config.sh
+++ Template-Config.sh
@@ -3,7 +3,9 @@
 ##################################################
 #  Enable/Disable compile-time options as needed #
 ##################################################
-<<<<<<< HEAD
+
+#--------------------------------------- SOLAS additions
+
 #STARS
 #BLACKHOLES
 #BURST_MODE
@@ -23,6 +25,15 @@
 #BH_JET_FEEDBACK
 #BH_JET_REFINEMENT
 #OUTPUT_REFBHCOUNTER
+
+#AGORA_SF
+#JEANS_SF #Jeans length based SF
+#JEANS_MASS_BASED #Jeans mass based SF
+
+#USE_GRACKLE
+#GRACKLE_CHEMISTRY=0 #Curretly only grackle mode=0 (lookup tables) with no chemistry network is supported
+
+#METALS #TODO: Currently gas cells have constant metallicity
 
 #--------------------------------------- Basic operation mode of code; default: 3d with 6 particle types; type 0: gas >0: only gravitationally interacting
 #NTYPES=6                      # number of particle types
@@ -73,7 +84,7 @@
 
 #--------------------------------------- non-standard phyiscs
 #COOLING                       # Simple primordial cooling
-#LOW_TEMP_COOLING              # Maschenko et al. 2008
+#LOW_TEMP_COOLING              # Maschenko et al. 2008 # Don't use while USE_GRACKLE
 #ENFORCE_JEANS_STABILITY_OF_CELLS  # this imposes an adaptive floor for the temperature
 #USE_SFR                       # Star formation model, turning dense gas into collisionless partices
 #SFR_KEEP_CELLS                # Do not distroy cell out of which a star has formed
@@ -125,110 +136,6 @@
 #ISO_FRACTION=0.9              # fraction in dark matter in isothermal sphere potential
 
 #--------------------------------------- Static Hernquist Potential (This is setup for the nuclear star cluster)
-||||||| empty tree
-=======
-
-#--------------------------------------- Basic operation mode of code; default: 3d with 6 particle types; type 0: gas >0: only gravitationally interacting
-#NTYPES=6                      # number of particle types
-#TWODIMS                       # 2d simulation
-#ONEDIMS                       # 1d simulation
-#ONEDIMS_SPHERICAL             # 1d spherically symmetric simulation
-
-#--------------------------------------- Computational box and boundaries; default: cubic, periodic box
-#LONG_X=10.0                   # stretch x extent of box by given factor
-#LONG_Y=2.0                    # stretch y extent of box by given factor
-#LONG_Z=10.0                   # stretch z extent of box by given factor
-#REFLECTIVE_X=1 #=2            # X Boundary; 1: Reflective, 2: Inflow/Outflow; not active: periodic
-#REFLECTIVE_Y=1 #=2            # Y Boundary; 1: Reflective, 2: Inflow/Outflow; not active: periodic
-#REFLECTIVE_Z=1 #=2            # Z Boundary; 1: Reflective, 2: Inflow/Outflow; not active: periodic
-
-#--------------------------------------- Hydrodynamics; default: GAMMA=5/3 ideal hydrodynamics
-#NOHYDRO                       # No hydrodynamics calculation
-#GAMMA=1.4                     # Adiabatic index of gas; 5/3 if not set
-#ISOTHERM_EQS                  # Isothermal gas
-#PASSIVE_SCALARS=3             # number of passive scalar fields advected with fluid (default: 0)
-#NO_SCALAR_GRADIENTS           # disables time and spatial extrapolation for passive scalar fields (use only if you know why you're doing this)
-
-#--------------------------------------- Magnetohydrodynamics
-#MHD                           # Master switch for magnetohydrodynamics
-#MHD_POWELL                    # Powell div(B) cleaning scheme for magnetohydrodynamics
-#MHD_POWELL_LIMIT_TIMESTEP     # Timestep constraint due to Powell cleaning scheme
-#MHD_SEEDFIELD                 # Uniform magnetic seed field of specified orientation and strength set up after reading in IC
-
-#--------------------------------------- Riemann solver; default: exact Riemann solver
-#RIEMANN_HLLC                  # HLLC approximate Riemann solver
-#RIEMANN_HLLD                  # HLLD approximate Riemann solver (required to use for MHD)
-
-#--------------------------------------- Mesh motion and regularization; default: moving mesh
-#VORONOI_STATIC_MESH           # static mesh
-#VORONOI_STATIC_MESH_DO_DOMAIN_DECOMPOSITION  # for VORONOI_STATIC_MESH force domain decomposition if there exist non-gas particles
-#REGULARIZE_MESH_CM_DRIFT      # Mesh regularization; Move mesh generating point towards center of mass to make cells rounder.
-#REGULARIZE_MESH_CM_DRIFT_USE_SOUNDSPEED  # Limit mesh regularization speed by local sound speed
-#REGULARIZE_MESH_FACE_ANGLE    # Use maximum face angle as roundness criterion in mesh regularization
-
-#--------------------------------------- Refinement and derefinement; default: no refinement/derefinement; criterion: target mass
-#REFINEMENT_SPLIT_CELLS        # Refinement
-#REFINEMENT_MERGE_CELLS        # Derefinement
-#REFINEMENT_VOLUME_LIMIT       # Limit the volume of cells and the maximum volume difference between neighboring cels
-#JEANS_REFINEMENT              # Refinement criterion to ensure Jeans stability of cells
-#REFINEMENT_HIGH_RES_GAS       # Refinement criterion for high-resolution region in zoom simulation
-#NODEREFINE_BACKGROUND_GRID    # Do not de-refine low-res gas cells in zoom simulations
-#OPTIMIZE_MESH_MEMORY_FOR_REFINEMENT  # deletes the mesh structures not needed for refinement/derefinemet to lower the peak memory consumption
-
-#--------------------------------------- non-standard phyiscs
-#COOLING                       # Simple primordial cooling
-#ENFORCE_JEANS_STABILITY_OF_CELLS  # this imposes an adaptive floor for the temperature
-#USE_SFR                       # Star formation model, turning dense gas into collisionless partices
-#SFR_KEEP_CELLS                # Do not distroy cell out of which a star has formed
-
-#--------------------------------------- Gravity treatment; default: no gravity
-#SELFGRAVITY                   # gravitational intraction between simulation particles/cells
-#HIERARCHICAL_GRAVITY          # use hierarchical splitting of the time integration of the gravity
-#CELL_CENTER_GRAVITY           # uses geometric centers to calculate gravity of cells, only possible with HIERARCHICAL_GRAVITY
-#NO_GAS_SELFGRAVITY            # switch off gas self-gravity in tree
-#GRAVITY_NOT_PERIODIC          # gravity is not treated periodically
-#ALLOW_DIRECT_SUMMATION        # Performed direct summation instead of tree-based gravity if number of active particles < DIRECT_SUMMATION_THRESHOLD (= 3000 unless specified differently here)
-#DIRECT_SUMMATION_THRESHOLD=1000  # Overrides maximum number of active particles for which direct summation is performed instead of tree based calculation
-#EXACT_GRAVITY_FOR_PARTICLE_TYPE=4  #N-squared fashion gravity for a small number of particles of the given type
-#EVALPOTENTIAL                 # computes gravitational potential
-
-#--------------------------------------- TreePM Options; default: no Particle-Mesh
-#PMGRID=512                    # Enables particle mesh; number of cells used for grid in each dimension
-#ASMTH=1.25                    # This can be used to override the value assumed for the scale that defines the long-range/short-range force-split in the TreePM algorithm. The default value is 1.25, in mesh-cells.
-#RCUT=6.0                      # This can be used to override the maximum radius in which the short-range tree-force is evaluated (in case the TreePM algorithm is used). The default value is 4.5, given in mesh-cells.
-#PM_ZOOM_OPTIMIZED             # Particle-mesh calculation that is optimized for cosmological zoom simulations; disable for cosmological volume simulations.
-#PLACEHIGHRESREGION=2          # Places a second, high-resolution PM grid; number encodes high-res particle types from which the extent of this second PM grid is calculated.
-#ENLARGEREGION=1.1             # Factor by which high res region is increased with respect to max-min position of high-res particles
-#GRIDBOOST=2                   # Factor by which PMGRID is increased in non-periodic (or high res) PM calculation (if not set, code uses 2)
-#FFT_COLUMN_BASED              # Use column-based FFT; slightly slower, but necessary to achieve good load-balancing if number of tasks larger than PMGRID (usually the case for very large runs)
-
-#--------------------------------------- Gravity softening
-#NSOFTTYPES=4                  # Number of different softening values to which particle types can be mapped.
-#MULTIPLE_NODE_SOFTENING       # If a tree node is to be used which is softened, this is done with the softenings of its different mass components
-#INDIVIDUAL_GRAVITY_SOFTENING=2+4  # bitmask with particle types where the softenig type should be chosen with that of parttype 1 as a reference type
-#ADAPTIVE_HYDRO_SOFTENING      # Adaptive softening of gas cells depending on their size
-#NSOFTTYPES_HYDRO=64           # Overrides number of discrete softening values for gas cellls when ADAPTIVE_HYDRO_SOFTENING (default is 64)
-
-#--------------------------------------- External gravity; default: no external potential
-#EXTERNALGRAVITY               # master switch for external potential
-#EXTERNALGY=0.0                # constant external gravity in y direction
-
-#--------------------------------------- Static NFW Potential
-#STATICNFW                     # static gravitational Navarro-Frenk-White (NFW) potential
-#NFW_C=12                      # concentration parameter of NFW potential
-#NFW_M200=100.0                # mass causing the NFW potential
-#NFW_Eps=0.01                  # softening of NFW potential
-#NFW_DARKFRACTION=0.87         # fraction in dark matter in NFW potential
-
-#--------------------------------------- Static Isothermal Sphere Potential
-#STATICISO                     # static gravitational isothermal sphere potential
-#ISO_M200=100.0                # mass causing the isothermal sphere potential
-#ISO_R200=160.0                # radius of the isothermal sphere potential
-#ISO_Eps=0.1                   # softening of isothermal sphere potential
-#ISO_FRACTION=0.9              # fraction in dark matter in isothermal sphere potential
-
-#--------------------------------------- Static Hernquist Potential
->>>>>>> master
 #STATICHQ                      # static gravitational Hernquist potential
 #HQ_M200=186.015773            # mass causing the Hernquist potential
 #HQ_C=10.0                     # concentration parameter of Hernquist potential
